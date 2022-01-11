ce7ad7f4e039b69092483ea78af70e9001fc15e7
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-11 14:53:35 -0300

    Criar Header

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './componente/Header';
+import './index.css';
+
+ReactDOM.render(
+  <Header title="Lead Collector Login"></Header>,
+  document.getElementById('root')
+);
