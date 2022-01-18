ff8db6ff52b620989766b2653ef4c61588ff7b1c
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-18 16:22:13 -0300

    feat: Criar dashboard

--- src/componente/pages/dashboard/index.js
+++ src/componente/pages/dashboard/index.js
@@ -1,14 +1,87 @@
-import react, {Component} from "react";
+import react, { Component } from "react";
+import {
+  Table,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Alert,
+  Card,
+} from "reactstrap";
 
-class Dashboard extends Component{
-    render(){
-        return(
-            <div>
-                <h1>DASHBOARD</h1>
-                <h2>Você foi logado com sucesso.</h2>
-            </div>
-        );
-    }
+class ListLead extends Component {
+  render() {
+    return (
+      <Table className="table-bordered text-center">
+        <thead className="thead-dark">
+          <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Obs.</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+      </Table>
+    );
+  }
+}
+class FormLead extends Component {
+  render() {
+    return (
+      <Form>
+        <FormGroup>
+          <div className="form-row">
+            <Label for="nome">Nome</Label>
+            <Input
+              id="nome"
+              type="text"
+              placeholder="Informe o nome do lead"
+            />
+          </div>
+        </FormGroup>
+        <FormGroup>
+          <div className="form-row">
+            <Label for="email">Email</Label>
+            <Input
+              id="email"
+              type="text"
+              placeholder="Informe o email do lead"
+            />
+          </div>
+        </FormGroup>
+        <FormGroup>
+          <div className="form-row">
+            <Label for="observacoes">Observações</Label>
+            <Input
+              id="observacoes"
+              type="text"
+              placeholder="Informe aqui as observacoes"
+            />
+          </div>
+        </FormGroup>
+        <Button color="primary" block >Atualizar</Button>
+      </Form>
+    );
+  }
+}
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-md-6 my-3">
+              <h2 className="font-weight-bold text-center" >ATUALIZAR LEADS</h2>
+              <FormLead />
+          </div>
+          <div className="col-md-6 my-3">
+            <h2 className="font-weight-bold text-center" >LISTA LEADS</h2>
+            <ListLead />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
