b7c4d4b8b0f4dae4c7f60d0c90819d074c31c8e6
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-18 11:00:28 -0300

    feat: Integrar landing page com a api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import react, { Component } from "react";
-import Login from "./componente/pages/login";
+//import Login from "./componente/pages/login";
 import "./App.css";
 import { } from "reactstrap";
 import Card from "./componente/Card";
-//import CadastroLead from "./componente/pages/cadastroLead";
+import CadastroLead from "./componente/pages/cadastroLead";
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <Card>
-          <Login></Login>
+          <CadastroLead></CadastroLead>
         </Card>
       </div>
     );
--- src/componente/pages/cadastroLead.js
+++ src/componente/pages/cadastroLead.js
@@ -1,31 +1,77 @@
-import react, {Component} from "react";
-import {Form, FormGroup, Label, Input, Button} from 'reactstrap'
-import Header from '../Header';
-
+import react, { Component } from "react";
+import { Form, FormGroup, Label, Input, Button, Alert } from "reactstrap";
+import Header from "../Header";
 
 export default class CadastroLead extends Component {
-    render(){
-        return(
-            <div>
-                <Header title="Cadastro Leads"></Header>
-                <hr/>
-                <Form className="forms">
-                    <FormGroup>
-                        <Label for="nome">Nome</Label>
-                        <Input type="text" id="nome" placeholder="Informe o seu nome: "/>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="email">Email</Label>
-                        <Input type="text" id="email" placeholder="Informe o seu email: "/>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="descricao">Descrição</Label>
-                        <textarea placeholder="Faça a descrição aqui:  " class="form-control"></textarea>
-                    </FormGroup>
-                    <Button color="primary" block>Entrar</Button>
-                </Form>
-            </div>
-            
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: this.props.state ? this.state.message : '',
+    };
+  }
+
+  save = () => {
+    const url = "http://localhost:8080/leads";
+    const data = {
+      nome: this.nome,
+      email: this.email,
+      observacoes: this.observacoes,
+    };
+    const requestInfo = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    };
+
+    fetch(url, requestInfo)
+      .then((response) => {
+        return response
+      })
+      .catch(e => {
+        this.setState({ message: e.message });
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header title="Cadastro Leads"></Header>
+        <hr/>
+        <Form className="forms">
+          <FormGroup>
+            <Label for="nome">Nome</Label>
+            <Input
+              type="text"
+              id="nome"
+              onChange={(e) => (this.nome = e.target.value)}
+              placeholder="Informe o seu nome: "
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="email">Email</Label>
+            <Input
+              type="text"
+              id="email"
+              onChange={(e) => (this.email = e.target.value)}
+              placeholder="Informe o seu email: "
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="observacoes">Observações</Label>
+            <textarea
+              id="observacoes"
+              onChange={(e) => (this.observacoes = e.target.value)}
+              placeholder="Utilize esse campo para adicionar observações:  "
+              className="form-control"
+            ></textarea>
+          </FormGroup>
+          <Button color="primary" block onClick={this.signIn}>
+            Entrar
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
