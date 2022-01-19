031f4659a6fbe00304a0de68ddca943a8508ab1f
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-19 09:11:36 -0300

    Adicionar rota para a landing page

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import PrivateRoute from "./auth";
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from "./componente/pages/login";
 import Dashboard from "./componente/pages/dashboard"
+import CadastroLead from "./componente/pages/cadastroLead";
 
 const Routes= () => (
     <Router>
         <Switch>
             <Route exact path='/' component={Login}/>
             <PrivateRoute path='/dashboard' component={Dashboard} />
+            <Route exact path='/landing' component={CadastroLead}/>
         </Switch>
     </Router>
 )
